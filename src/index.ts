48d723939d0ff46588083b3ec2fcbcd71abe472f
Pedro-Augusto-Barbosa-Aparecido <pedr.augustobarbosa.aparecido.gmail.com>
2021-07-14 22:12:04 -0300

    Configure Heroku

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
-    console.log("Server running on PORT 8080!");
+app.listen(process.env.PORT || 3333, () => {
+    console.log(`Server running on PORT ${process.env.PORT || 3333}!`);
 });
